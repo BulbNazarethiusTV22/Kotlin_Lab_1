7951784fc0156eabc6262517ceb395151d3c1540
Naz Bulb <nazbulb@example.com>
2025-12-20 19:01:23 +0200

    Initial commit for lab 5 energy calculator

--- app/src/main/java/com/example/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -10,22 +10,28 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlin.math.exp
+import kotlin.math.pow
+import kotlin.math.sqrt
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { FuelCalculatorScreen() }
+        setContent { EnergyCalculatorScreen() }
     }
 }
 
 @Composable
-fun FuelCalculatorScreen() {
+fun EnergyCalculatorScreen() {
     val scrollState = rememberScrollState()
 
-    var coalB by remember { mutableStateOf("") }
-    var oilB by remember { mutableStateOf("") }
-    var gasB by remember { mutableStateOf("") }
+    var woc by remember {mutableStateOf("")}
+    var tboc by remember { mutableStateOf("") }
+    var zpera by remember { mutableStateOf("") }
+    var zperp by remember { mutableStateOf("") }
     var result by remember { mutableStateOf("") }
+
 
     Column(
         modifier = Modifier
@@ -33,52 +39,69 @@ fun FuelCalculatorScreen() {
             .padding(16.dp)
             .verticalScroll(scrollState)
     ) {
-        Text("Введіть значення В (витрат палива):", style = MaterialTheme.typography.titleLarge)
+        Text("Введення даних для розрахунку:", style = MaterialTheme.typography.titleLarge)
 
         OutlinedTextField(
-            value = coalB,
-            onValueChange = { coalB = it },
-            label = { Text("Вугілля (Ввуг)") },
+            value = tboc,
+            onValueChange = { tboc = it },
+            label = { Text("Середня тривалість відновлення") },
             modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
         )
+
         OutlinedTextField(
-            value = oilB,
-            onValueChange = { oilB = it },
-            label = { Text("Мазут (Вмаз)") },
+            value = woc,
+            onValueChange = { woc = it },
+            label = { Text("Частота відмов одноколової системи") },
             modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
         )
+
         OutlinedTextField(
-            value = gasB,
-            onValueChange = { gasB = it },
-            label = { Text("Газ (Вгаз)") },
+            value = zpera,
+            onValueChange = { zpera = it },
+            label = { Text("Зпер.а (грн/кВт*год)") },
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+        )
+
+        OutlinedTextField(
+            value = zperp,
+            onValueChange = { zperp = it },
+            label = { Text("Зпер.п (грн/кВт*год)") },
             modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
         )
 
         Spacer(Modifier.height(12.dp))
 
+
         Button(onClick = {
             try {
-                val coal = coalB.toDouble()
-                val oil = oilB.toDouble()
+                val wocVal = woc.toDouble()
+                val tbocVal = tboc.toDouble()
+                val zperaVal = zpera.toDouble()
+                val zperpVal = zperp.toDouble()
 
-                val coalE = 150 * coal * 20.47 / 1000000
-                val oilE = 0.57 * oil * 39.48 / 1000000
-                var total = oilE + coalE
+
+                val kaoc = wocVal * tbocVal / 8760;
+                val kpoc = 1.2 * 43/8760;
+                val wdk = 2 * wocVal * (kaoc + kpoc);
+                val wdc = wdk + 0.02;
+
+                val wneda = 0.01 * 45 / 1000 * 5.12 * 1000 * 6451;
+                val wnedp = 4 * 5.12 * 6451;
+                val zper = wneda * zperaVal + wnedp * zperpVal;
+
 
                 result = """
-                    🔥 Валовий викид при спалюванні вугілля:
-                    ${"%.2f".format(coalE)} т;
-                    
-                    🔥 Валовий викид при спалюванні мазуту:
-                    ${"%.2f".format(oilE)} т;
-                    
-                    При спалюванні природного газу тверді частинки відсутні;
-                    
-                    🔥 Сумарний валовий викид при спалюванні усіх видів палива:
-                    ${"%.2f".format(total)} т.
+                    Коефіцієнт аварійного простою одноколової системи = ${"%.2f".format(kaoc * 10000)} * 10^(-4);
+                    Коефіцієнт планового простою одноколової системи = ${"%.2f".format(kpoc * 10000)} * 10^(-4);
+                    Частота відмов одночасно двох кіл двоколової системи = ${"%.2f".format(wdk * 10000)} * 10^(-4);
+                    Частота відмов двоколової системи з урахуванням секційного вимикача = ${"%.4f".format(wdc)}
+
+                    Математичне сподівання аварійного недовідпущення електроенергії = ${"%.2f".format(wneda)} кВт*год;
+                    Математичне сподівання планового недовідпущення електроенергії = ${"%.2f".format(wnedp)} кВт*год;
+                    Математичне сподівання збитків від переривання електропостачання = ${"%.2f".format(zper)} грн.
                 """.trimIndent()
             } catch (e: Exception) {
-                result = "❗ Перевірь правильність введення чисел"
+                result = "Помилка у введенні даних"
             }
         }) {
             Text("Розрахувати")
